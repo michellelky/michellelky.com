55c631cf96d07920fee5c757e956175c78de820a
Michelle Lau <michellelkys@gmail.com>
2023-09-28 01:02:04 +0800

    Fix vercel missing directory

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  experimental: {
+    nftTracing: true,
+  },
+};
+
+module.exports = nextConfig;
