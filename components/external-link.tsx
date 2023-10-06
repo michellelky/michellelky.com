e359bc8c4b579785d4255d160678519c374ad42d
Michelle Lau <michellelkys@gmail.com>
2023-10-06 17:29:59 +0800

    Update note item style

--- components/external-link.tsx
+++ components/external-link.tsx
@@ -4,14 +4,21 @@ interface Props {
   href?: string | undefined;
   children?: any;
   className?: string;
+  title?: string;
 }
-export default function ExternalLink({ href, children, className }: Props) {
+export default function ExternalLink({
+  href,
+  children,
+  className,
+  title,
+}: Props) {
   return (
     <a
       href={href}
       target="_blank"
       rel="noreferrer noopener"
       className={className}
+      title={title}
     >
       {children}
     </a>
--- components/note-item/note-item.tsx
+++ components/note-item/note-item.tsx
@@ -1,42 +1,91 @@
 "use client";
 
 import React from "react";
-import clsx from "clsx";
 
 import styles from "./note-item.module.css";
+import { FaExternalLinkSquareAlt, FaSpotify, FaYoutube } from "react-icons/fa";
+import ExternalLink from "../external-link";
+import clsx from "clsx";
+
+const flags: { [key: string]: string } = {
+  italian: "🇮🇹",
+  french: "🇫🇷",
+  spanish: "🇪🇸",
+};
 
 interface Props {
   title: string;
   content: string;
   translation: string;
   date: string;
-  source?: string;
+  source?: { type: string; url: string };
   tags?: string[];
 }
 
-export default function NoteItem({ title, content, translation, tags }: Props) {
+export default function NoteItem({
+  title,
+  content,
+  translation,
+  tags,
+  source,
+}: Props) {
   const [transVisible, setTransVisible] = React.useState(false);
 
   function toggleContent() {
     setTransVisible(!transVisible);
   }
 
+  function getCountryFlag(t: string) {
+    return flags[t] || "";
+  }
+
+  function getSourceIcon(type: string) {
+    const SIZE = "28px";
+    if (type === "youtube") {
+      return <FaYoutube size={SIZE} />;
+    }
+    if (type === "spotify") {
+      return <FaSpotify size={SIZE} />;
+    }
+    return <FaExternalLinkSquareAlt size={SIZE} />;
+  }
+
   return (
-    <div className={styles.container} onClick={toggleContent}>
-      <p className={styles.header}>{content}</p>
-      {transVisible && (
-        <div className={styles.highlightWrapper}>
-          <p className={clsx(styles.header, styles.highlight)}>{translation}</p>
+    <div className={styles.container}>
+      <button className={styles.card} onClick={toggleContent}>
+        <p className={styles.header}>{content}</p>
+        {transVisible && (
+          <div className={styles.highlightWrapper}>
+            <p className={styles.highlight}>{translation}</p>
+          </div>
+        )}
+        <p className={styles.body}>{title}</p>
+
+        {tags &&
+          tags.map((t) => (
+            <p key={t} className={styles.category}>
+              {getCountryFlag(t)}
+            </p>
+          ))}
+
+        <div className={styles.tipWrapper}>
+          <div className={clsx(styles.tip, transVisible ? styles.open : "")}>
+            {transVisible ? "Hide" : "Translate"}
+          </div>
+        </div>
+      </button>
+
+      {!!source && (
+        <div className={styles.actionWrapper}>
+          <ExternalLink
+            className={styles.action}
+            href={source.url}
+            title={source.type}
+          >
+            {getSourceIcon(source.type)}
+          </ExternalLink>
         </div>
       )}
-      <p className={styles.body}>{title}</p>
-
-      {tags &&
-        tags.map((t) => (
-          <p key={t} className={styles.category}>
-            #{t}
-          </p>
-        ))}
     </div>
   );
 }
